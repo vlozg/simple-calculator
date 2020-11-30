f48bec1b3ec80165bae2b4f6840e492b1fe16ab3
long vu <hoanglongvudang@gmail.com>
2020-12-01 03:46:08 +0700

    Add functionality for least-order components

--- script/calculator.js
+++ script/calculator.js
@@ -4,7 +4,86 @@ class App extends React.Component {
     }
 
     render() {
-        return (<p>Hello by Long Vu from React</p>);
+        return (
+        <React.Fragment>
+            <DisplaySection/>
+            <ButtonSection/>
+        </React.Fragment>
+        );
+    }
+}
+
+class DisplaySection extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <MainDisplay/>
+            <div>
+                <Button className="binary" value="B"/>
+                <SubDisplay value="0"/>
+            </div>
+            <div>
+                <Button className="hexa" value="H"/>
+                <SubDisplay value="0"/>
+            </div>
+        </React.Fragment>
+        );
+    }
+}
+
+class SubDisplay extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <input type="text" value={this.props.value}/>
+        </React.Fragment>
+        );
+    }
+}
+
+class MainDisplay extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <input type="text" value={this.props.value}/>
+        </React.Fragment>
+        );
+    }
+}
+
+class ButtonSection extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <Button className="button" value="Test"/>
+        );
+    }
+}
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <input className={"button "+this.props.className} type="button" value={this.props.value}/>
+        );
     }
 }
 
