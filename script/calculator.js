20d2e6a9a0bb8ee39ba88927f3ea8565065e9426
long vu <hoanglongvudang@gmail.com>
2020-12-01 06:20:55 +0700

    Complete init all components

--- script/calculator.js
+++ script/calculator.js
@@ -1,6 +1,6 @@
 class App extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
@@ -15,7 +15,7 @@ class App extends React.Component {
 
 class DisplaySection extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
@@ -37,7 +37,7 @@ class DisplaySection extends React.Component {
 
 class SubDisplay extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
@@ -51,7 +51,7 @@ class SubDisplay extends React.Component {
 
 class MainDisplay extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
@@ -65,19 +65,57 @@ class MainDisplay extends React.Component {
 
 class ButtonSection extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            buttons: [
+                {id: "reverse",symbol:"±"}, 
+                {id: "dot", symbol:"."}, 
+                {id: "0", symbol:"0"}, 
+                {id: "1", symbol:"1"}, 
+                {id: "2", symbol:"2"}, 
+                {id: "3", symbol:"3"}, 
+                {id: "4", symbol:"4"}, 
+                {id: "5", symbol:"5"}, 
+                {id: "6", symbol:"6"}, 
+                {id: "7", symbol:"7"}, 
+                {id: "8", symbol:"8"}, 
+                {id: "9", symbol:"9"}, 
+                {id: "A", symbol:"A"}, 
+                {id: "B", symbol:"B"}, 
+                {id: "C", symbol:"C"}, 
+                {id: "D", symbol:"D"}, 
+                {id: "E", symbol:"E"}, 
+                {id: "F", symbol:"F"}, 
+                {id: "plus", symbol:"+"}, 
+                {id: "minus", symbol:"-"}, 
+                {id: "times", symbol:"×"}, 
+                {id: "divide", symbol:"÷"}, 
+                {id: "calc", symbol:"="}, 
+                {id: "shift_l", symbol:"<<"}, 
+                {id: "shift_r", symbol:">>"}, 
+                {id: "rol_r", symbol:"RR"}, 
+                {id: "rol_l", symbol:"RL"}, 
+                {id: "and", symbol:"AND"},
+                {id:"or", symbol:"OR"},
+                {id: "xor", symbol:"XOR"},
+                {id: "not", symbol:"NOT"},
+                {id: "clear", symbol:"C."}
+            ]
+        }
     }
 
     render() {
         return (
-        <Button className="button" value="Test"/>
+        <div className="button-wrap">
+        {this.state.buttons.map((x) => <Button key={x.id} className={x.id} value={x.symbol}/>)}
+        </div>
         );
     }
 }
 
 class Button extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
